acbfc07672e3f51ec5d1c7bcae166c9603d04b8a
Ethan Chen <intervigil@gmail.com>
2019-02-28 22:16:46 -0800

    aidl: bluetooth: Convert bluetooth-impl to read QC MAC address
    * btaddr_extract cannot be used anymore since ro.boot.btmacaddr is not
      writable by vendor domains. bluetooth_prop is off-limits to vendor
      domains now.
    * Have the Bluetooth HAL impl directly read
      /persist/bluetooth/.bt_nv.bin itself and pull the MAC address out.
    
    Change-Id: If4b2b99345eb470c140523906a56299ec5decc9a

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -5,3 +5,8 @@
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     hardware/essential/sepolicy/qti/vendor
+
+ifeq (,$(filter device/qcom/sepolicy-legacy-um/legacy/vendor/common device/qcom/sepolicy_vndr/legacy-um/legacy/vendor/common, $(BOARD_VENDOR_SEPOLICY_DIRS)))
+BOARD_SEPOLICY_M4DEFS += \
+    persist_bluetooth_file=vendor_persist_bluetooth_file
+endif
