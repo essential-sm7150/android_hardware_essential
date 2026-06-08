fc7a066f2c26370acbbcb2691363a13e7d27d8b0
Giovanni Ricca <giovanniricca@proton.me>
2026-06-08 16:00:43 +0200

    aidl: bluetooth: Rebrand to essential
    Change-Id: I95d5dd58034667b4a4a6eb4030f0c98bcb29c3ad

--- aidl/bluetooth/vendor_interface.cc
+++ aidl/bluetooth/vendor_interface.cc
@@ -17,7 +17,7 @@
 
 #include "vendor_interface.h"
 
-#define LOG_TAG "android.hardware.bluetooth.service.default"
+#define LOG_TAG "android.hardware.bluetooth.service.essential"
 #include <cutils/properties.h>
 #include <dlfcn.h>
 #include <fcntl.h>
